8c836a67febd6ca11b82fd19a39487c6bd69374e
maryk.maddox@gmail.com <maryk.maddox@gmail.com>
2026-01-08 15:51:45 -0500

    Function Declaration

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ from cmd import PROMPT
 
 from dotenv import load_dotenv
 
+from functions.call_function import available_functions
 from prompts import system_prompt
+
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
@@ -41,7 +43,9 @@ def main():
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
-        config=types.GenerateContentConfig(system_instruction=system_prompt),
+        config=types.GenerateContentConfig(
+            tools=[available_functions], system_instruction=system_prompt
+        ),
     )
     if response.usage_metadata == None:
         raise RuntimeError("failed API request")
@@ -55,9 +59,14 @@ def main():
         print(f"Response tokens: {y}")
         print("Response:")
         print(response.text)
-    else:
+
+    if not response.function_calls:
         print("Response:")
         print(response.text)
+        return
+
+    for function_call in response.function_calls:
+        print(f"Calling function: {function_call.name}({function_call.args})")
 
 
 if __name__ == "__main__":
